837e3f74236c1d43f9b7e7c31533d5e3d6d21708
Ouzw <ouzhiwei@jointcharging.com>
2024-09-25 21:05:24 +0800

    feat(tree.lua): focus file when open nvim-tree

--- lua/config/tree.lua
+++ lua/config/tree.lua
@@ -21,7 +21,8 @@ vim.keymap.set({"n"}, "<M-b>", function()
     if api.tree.is_tree_buf() then
         api.tree.close()
     else
-        api.tree.open()
+        -- api.tree.open()
+        api.tree.open({find_file = true, focus = true, })
     end
 end, {silent = true})
 -- vim.api.nvim_set_keymap('n', "<M-b>",  "<cmd>NvimTreeToggle<cr>", { noremap = true, silent = true })
