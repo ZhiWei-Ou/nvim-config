dae6da95f017c27fb0a8286098c0a13348347e89
Ouzw <ouzhiwei@jointcharging.com>
2024-10-03 10:23:16 +0800

    fix(cmake-tool): fix default CMakeBuild always failed

--- lua/config/nvim-cmake-tools.lua
+++ lua/config/nvim-cmake-tools.lua
@@ -4,7 +4,7 @@ require("cmake-tools").setup {
   ctest_command = "ctest", -- this is used to specify ctest command path
   cmake_use_preset = true,
   cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
-  cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE", "--no-warn-unused-cli", "-G\"Unix Makefiles\"" }, -- this will be passed when invoke `CMakeGenerate`
+  cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE", "--no-warn-unused-cli", "-GUnix Makefiles" }, -- this will be passed when invoke `CMakeGenerate`
   cmake_build_options = {"-j 10"}, -- this will be passed when invoke `CMakeBuild`
   -- support macro expansion:
   --       ${kit}
