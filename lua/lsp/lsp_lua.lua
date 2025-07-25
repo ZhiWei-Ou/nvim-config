b4439bffe76ddd17579e4581036d7763af9724fb
Ouzw <ouzw.mail@gmail.com>
2025-07-25 15:58:25 +0800

    chore: disable all lua lsp diagnostics.

--- lua/lsp/lsp_lua.lua
+++ lua/lsp/lsp_lua.lua
@@ -14,7 +14,21 @@ G.lsp_config.lua_ls.setup {
           'after_each',
           'pending',
         }
-      }
+      },
+      diagnostics = {
+        enable = false,
+        -- globals = { 'vim' },
+        -- disable = { 'inject-field' },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+      telemetry = {
+        enable = false,
+      },
     }
   },
   on_attach = G.lsp_general_on_attach,
