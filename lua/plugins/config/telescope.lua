5bab3fed8b0a382ddc8074bcfb6aa561d82a99f5
Ouzw <ouzw.mail@gmail.com>
2025-07-25 16:05:40 +0800

    feat(live_grep): add support for searching selected word in visual mode

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -56,6 +56,24 @@ require('telescope').setup {
   }
 }
 
+-- 获取 Visual 模式选中文本
+local function get_visual_selection()
+  local _, ls_row, ls_col = unpack(vim.fn.getpos("'<"))
+  local _, le_row, le_col = unpack(vim.fn.getpos("'>"))
+  local lines = vim.fn.getline(ls_row, le_row)
+  if #lines == 0 then return "" end
+
+  lines[#lines] = string.sub(lines[#lines], 1, le_col)
+  lines[1] = string.sub(lines[1], ls_col)
+  return table.concat(lines, '\n')
+end
+
+-- grep 选中文本
+function _G.live_grep_visual()
+  local text = get_visual_selection()
+  require('telescope.builtin').live_grep({ default_text = text })
+end
+
 -- 模糊搜索
 -- 搜索文件 {find_command={"fd", "--type", "f", "--strip-cwd-prefix"}}
 vim.api.nvim_set_keymap('n', '<C-p>',
@@ -63,6 +81,8 @@ vim.api.nvim_set_keymap('n', '<C-p>',
   { noremap = true, silent = true })
 -- 搜索匹配字符串
 vim.api.nvim_set_keymap('n', '<C-F>', '<cmd>lua require("telescope.builtin").live_grep()<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-F>', ':<C-U>lua live_grep_visual()<CR>',
   { noremap = true, silent = true })
 -- 工具集
 vim.api.nvim_set_keymap('n', '<C-K><C-p>', '<cmd>lua require("telescope.builtin").builtin{include_extensions = true}<CR>',
