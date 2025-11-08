36a9d349e338346c47751f2a71ef92299559450b
Ouzw <ouzw.mail@gmail.com>
2025-11-08 13:45:09 +0800

    feat(luasnip): add custom code snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -3,15 +3,17 @@
 @refer: https://github.com/L3MON4D3/LuaSnip
 ]]
 return {
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp",
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
 
     dependencies = { 'rafamadriz/friendly-snippets' },
 
-    config = function ()
+    config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
+
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
     end
 }
