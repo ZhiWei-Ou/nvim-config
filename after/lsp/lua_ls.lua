6d001a498172e2e0c7ee2a7e3e9027442ab48211
Ouzw <ouzw.mail@gmail.com>
2026-05-22 13:25:21 +0800

    chore: update .luarc.json

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -23,17 +23,6 @@ return {
   on_init = function(client)
     client.alias_name = 'Lua(lua_ls)'
   end,
-  on_attach = function(client, bufnr)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if
-          path ~= vim.fn.stdpath('config')
-          and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-      then
-        return
-      end
-    end
-  end,
   settings = {
     Lua = {
       runtime = {
@@ -48,22 +37,6 @@ return {
       },
       workspace = {
         checkThirdParty = false,
-        -- library = {
-        --     vim.env.VIMRUNTIME
-        -- Depending on the usage, you might want to add additional paths
-        -- here.
-        -- '${3rd}/luv/library'
-        -- '${3rd}/busted/library'
-        -- }
-        -- Or pull in all of 'runtimepath'.
-        -- NOTE: this is a lot slower and will cause issues when working on
-        -- your own configuration.
-        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
-        library = {
-          vim.api.nvim_get_runtime_file('', true),
-          vim.env.VIMRUNTIME .. '/lua',
-          vim.env.VIMRUNTIME .. '/lua/vim/lsp'
-        }
       }
     }
   }
